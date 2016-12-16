458c7adc438e5411cb0e5a7f9ea186e905cb84f0
kritsada <yi.kritsada_st@tni.ac.th>
2016-12-16 04:33:44 +0000

    update

--- Hw2.cc
+++ Hw2.cc
@@ -1,8 +1,14 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "time.h"
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
-{
-
+int main() {
+    int random,result;
+    std::srand(time(NULL));
+    for(int i=0;i<100;i++)
+    {
+    std::cout << "The nubmer is "<<rand()%100+1 << std::endl;
+    }
 }
