70d99c9adace402e8369481a927e191041e258f3
Thanawan <su.tanawn_st@tni.ac.th>
2016-12-16 04:42:38 +0000

    UpdateData

--- Hw2.cc
+++ Hw2.cc
@@ -1,8 +1,15 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "time.h"
 
-int main()
-{
-
-}
+ #include <iostream>
+ #include <cstdlib>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <time.h>
+  
+ int main() {
+     int random,result;
+     std::srand(time(NULL));
+     for(int i=0;i<6;i++)
+     {
+     std::cout <<rand()%10+1 << std::endl;
+     }
+ }
